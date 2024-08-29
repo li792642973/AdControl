e6bbae6a75ea1f9712cd4bf74d9875359b98f2c4
li792642973 <>
2024-08-29 18:48:00 +0800

    v 2.0.1

--- lib_ad/src/main/java/com/exp/ad/Ad1Control.java
+++
@@ -1,8 +0,0 @@
-package com.exp.ad;
-
-import org.ad.annotation.Adc;
-
-@Adc(name = "XAd1", sdk = false)
-public class Ad1Control {
-
-}
--- lib_ad/src/main/java/com/exp/ad/Ad2Control.java
+++
@@ -1,8 +0,0 @@
-package com.exp.ad;
-
-import org.ad.annotation.Adc;
-
-@Adc(name = "XAd2", sdk = false)
-public class Ad2Control {
-
-}
